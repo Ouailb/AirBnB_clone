34573f0c2aaa73fd604a6e478f2fa01344e6ae94
MUSTAPHA ESSALHI <essalhimu@gmail.com>
2023-11-01 20:20:43 +0100

    change some lines and fix an issue

--- models/base_model.py
+++ models/base_model.py
@@ -11,19 +11,27 @@ class BaseModel:
     """
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
-        self.updated_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
-        
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
     def __str__(self):
-        return f" BaseModel {self.id} {self.__dir__}"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
     
     def save(self):
-        self.updated_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+        self.updated_at = datetime.now()
     
     def to_dict(self):
-        return {
-                    "id": self.id,
-                    "created_at": self.created_at,
-                    "updated_at": self.updated_at,
-                    "__class__": self.__class__.__name__
-                }
+        # return {
+        #             'id': self.id,
+        #             'created_at': self.created_at.isoformat(),
+        #             'updated_at': self.updated_at.isoformat(),
+        #             '__class__': self.__class__.__name__,
+        #         }
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
+
+
+
