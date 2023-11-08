8d723ebf2f955535219688e63449be2d4baa5ebd
MUSTAPHA ESSALHI <essalhimu@gmail.com>
2023-11-08 20:11:06 +0100

    debug debug

--- console.py
+++ console.py
@@ -28,6 +28,8 @@ class HBNBCommand(cmd.Cmd):
             'update': self.do_update,
         }
 
+
+
     def default(self, args):
         if args:
             splitter = re.split(r"\.", args)
@@ -60,42 +62,40 @@ class HBNBCommand(cmd.Cmd):
         else:
             print("Nothing")
 
-    
+
+
     # def default(self, args):
     #     if args:
     #         splitter = re.split(r"\.", args)
     #         if len(splitter) == 2:
     #             class_name, part2 = splitter
-    #             print("class:", class_name)
-            
     #             command = part2.split("(")[0]
-    #             print(command)
-    #             # Extract text between parentheses and remove quotes
-    #             match = re.search(r'\((["\'])(.*?)\1\)', part2)
-    #             if match:
-    #                 id = match.group(2)
-    #                 id = str(id)
-    #                 print("Text between parentheses:",id)
-     
-    #                 if command in self.command_mapping:
-    #                   # Call the corresponding function
-    #                     result = self.command_mapping[command](class_name.id)
-    #                     print(result)
-    #                 else:
-    #                     print(f"** Method '{command}' doesn't exist in class '{class_name}' **")
+    #             match = re.search(r'\((.*?)\)', part2)
 
+    #             if match:
+    #                 id = match.group(1)
+    #                 if "{" in id and "}" in id:
+    #                     id = id.replace("'", '"')
+    #                     self.command_mapping[command](f"{class_name} {id}")
+    #                 else:
+    #                     if "}" in args:
+    #                         # Handle the case where arguments are a dictionary representation
+    #                         self.command_mapping[command](f"{class_name} {id}", args)
+    #                     else:
+    #                         # Handle the old format with separate attributes and values
+    #                         self.command_mapping[command](f"{class_name} {id}", args)
     #             else:
     #                 class_obj = self.command_mapping.get(command)
     #                 if class_obj and callable(class_obj):
     #                     result = class_obj(class_name)
     #                     if result is not None:
     #                         print(result)
-    #                 else:
-    #                     print(f"** Method '{command}' doesn't exist in class '{class_name}' **")
     #         else:
     #             print("Invalid format. There should be exactly one dot in the input string.")
     #     else:
     #         print("Nothing")
+
+
 
 
 
@@ -257,106 +257,65 @@ class HBNBCommand(cmd.Cmd):
         print(objs)
      
 
-    #  args = args.replace('"','')
-    #     print(args)
-    #     splitter = re.split(r" ", args)
-    #     if len(splitter) == 2:
-    #         class_name, part2 = splitter
-    #     print(class_name)
-    #     att = part2.split(',')
-    #     print(att[0],att[1],att[2])
-
-    def do_update(self, args):
-        """Update an instance based on class name and id"""
-        if not args:
-            print("** class name missing **")
-            return
-        args = args.replace('"','')
-        try:
-            splitter = re.split(r" ", args)
-            class_name, arg = splitter
-            # print(class_name)
-            attr = arg.split(',')
-            if len(arg) > 1:
-                obj_id = attr[0]
-                # print(obj_id)
-               
-                if len(arg) > 2:
-                    key = f"{class_name}.{obj_id}"
-                    # print(key)
-                    if key in storage.all():
-                        obj = storage.all()[key]
-                        attribute = attr[1]
-
-                        if len(arg) > 3:
-                            value = attr[2]
-                            try:
-                                value = eval(value)
-                            except (NameError, SyntaxError):
-                                pass
-
-                            setattr(obj, attribute, value)
-                            obj.save()
-                        else:
-                            print("** value missing **")
-                            return
-                    else:
-                        print("** no instance found **")
-                else:
-                    print("** attribute name missing **")
-                    return
-            else:
-                print("** instance id missing **")
-        except Exception as e:
-            print("** class doesn't exist **")
-
-
 
     # def do_update(self, args):
     #     """Update an instance based on class name and id"""
-    #     print(args)
-    #     args = args.replace('"','')
-    #     print(args)
     #     if not args:
     #         print("** class name missing **")
     #         return
+    #     print(args)
+    #     args = args.replace('"','')
+    #     args = args.replace(',','')
+    #     print(args)
 
-    #     try:
-    #         args_list = args.split()
-    #         class_name = args_list[0]
+    #     splitter = re.split(r" ", args)
 
-    #         if len(args_list) > 1:
-    #             obj_id = args_list[1]
+    #     class_name = splitter[0]
+    #     if not class_name:
+    #         print("** class doesn't exist **")
+    #         return
+    #     print(class_name)
 
-    #             if len(args_list) < 3 :
-    #                 key = f"{class_name}.{obj_id}"
-    #                 print(key)
-    #                 if key in storage.all():
-    #                     obj = storage.all()[key]
-    #                     attribute = args_list[2]
+    #     arg = splitter[1:]
+    #     print(arg)
+    #     if len(arg) > 1:
+    #         obj_id = arg[0]
+    #         print(obj_id)
+            
+    #         if len(arg) > 2:
+    #             key = f"{class_name}.{obj_id}"
+    #             print(key)
+    #             if key in storage.all():
+    #                 obj = storage.all()[key]
+    #                 print(arg[0])
+    #                 attribute = arg[1]
+    #                 print(attribute)
+    #                 print(arg[2])
+    #                 print(len(arg))
 
-    #                     if len(args_list) > 3:
-    #                         value = args_list[3]
-    #                         try:
-    #                             value = eval(value)
-    #                         except (NameError, SyntaxError):
-    #                             pass
-
-    #                         setattr(obj, attribute, value)
-    #                         obj.save()
-    #                     else:
-    #                         print("** value missing **")
-    #                         return
+    #                 if len(arg) >= 3:
+    #                     value = arg[2]
+    #                     print(value)
+    #                     try:
+    #                         value = eval(value)
+    #                     except (NameError, SyntaxError):
+    #                         pass
+    #                     print("attribute", attribute)
+    #                     print("obj", obj)
+    #                     print("value",value)
+    #                     setattr(obj, attribute, value)
+    #                     obj.save()
     #                 else:
-    #                     print("** no instance found **")
+    #                     print("** value missing **")
     #                     return
     #             else:
-    #                 print("** attribute name missing **")
-    #                 return
+    #                 print("** no instance found **")
     #         else:
-    #             print("** instance id missing **")
-    #     except Exception as e:
-    #         print("** class doesn't exist **")
+    #             print("** attribute name missing **")
+    #             return
+    #     else:
+    #         print("** instance id missing **")
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
